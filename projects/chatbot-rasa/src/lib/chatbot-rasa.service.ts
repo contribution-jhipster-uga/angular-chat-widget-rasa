ba89edef80571274d955a7ddeeadfdac9e21e226
Julien <julien.courtial07@gmail.com>
2019-03-15 12:38:23 +0100

    modifying chat service

--- projects/chatbot-rasa/src/lib/chatbot-rasa.service.ts
+++ projects/chatbot-rasa/src/lib/chatbot-rasa.service.ts
@@ -1,9 +1,49 @@
 import { Injectable } from '@angular/core';
+import io from 'socket.io-client';
+import { Observable } from 'rxjs/Observable';
 
 @Injectable({
   providedIn: 'root'
 })
-export class ChatbotRasaService {
+export class ChatService {
 
-  constructor() { }
+  private socket;
+
+  constructor() {
+
+  }
+
+  public connect(url: string){
+    this.socket = io(url);
+    this.socket.on('connect', () => {
+      console.log(`connect:${this.socket.id}`);
+      this.socket.emit('session_request', {'session_id':this.socket.id})
+    });
+    this.socket.on('session_confirm', (remote_id) => {
+      console.log(`session_confirm:${this.socket.id} session_id:${remote_id}`);
+    })
+    this.socket.on('connect_error', (error) => {
+      console.log(error);
+    });
+
+    this.socket.on('error', (error) => {
+      console.log(error);
+    });
+
+    this.socket.on('disconnect', (reason) => {
+      console.log(reason);
+    });
+  }
+
+  public sendMessage(message) {
+    this.socket.emit('user_uttered', {'message':message});
+  }
+
+  public getMessages = () => {
+        return Observable.create((observer) => {
+            this.socket.on('bot_uttered', (message) => {
+                observer.next(message);
+            });
+        });
+  }
 }
